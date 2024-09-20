0f2118ca439cc7889d397c308f754876fbd34525
babamovandrej <andrejbabamov@gmail.com>
2024-09-20 18:37:46 +0200

    Solve Exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const getPersonsAge = function (birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  };
+  
 
-};
+  const findTheOldest = function (people) {
+    return people.reduce((oldest, currentPerson) => {
+      const oldestAge = getPersonsAge(oldest.yearOfBirth, oldest.yearOfDeath);
+      const currentAge = getPersonsAge(
+        currentPerson.yearOfBirth,
+        currentPerson.yearOfDeath
+      );
+      return oldestAge < currentAge ? currentPerson : oldest;
+    });
+  };
 
-// Do not edit below this line
 module.exports = findTheOldest;
+
+
+
+
